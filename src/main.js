01fb20d825e8b5b51c54ca6164c424e4a3082523
11943 <1194362549@qq.com>
2020-04-26 19:24:55 +0800

    车永基--审批流程整合&删除config--0426

--- src/main.js
+++ src/main.js
@@ -3,9 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Vant from 'vant'
-// import axios from 'axios'
 import 'vant/lib/index.css'
-// import '../public/config'
 import { Select, Option, Table, TableColumn, Form, FormItem, Input, Tree, Button } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 // import './mock/index'
@@ -22,16 +20,9 @@ Vue.use(TableColumn)
   .use(Button)
 
 Vue.config.productionTip = false
-// const startApp = function () {
-// axios.get('../public/config.json').then((res) => {
-// 基础地址
-// Vue.prototype.BASE_URL = res.BASE_URL
+
 new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount('#app')
-// })
-// }
-
-// startApp()

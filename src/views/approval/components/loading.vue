6454b1ae621790619f314c04998add4ae256dbb5
xujintao <1101916341@qq.com>
2020-05-12 11:25:53 +0800

    徐锦涛---销售合同模块开发税金输入功能--0512

---
+++ src/views/approval/components/loading.vue
@@ -0,0 +1,29 @@
+/**
+*@author XuJinTao
+*@date 2020/5/11 16:45
+*@title loading
+*/
+<template>
+    <div :style="style">
+      <img src="../../../assets/image/twodevelopment/noorder.png"/>
+      <div>正在加载...</div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Loading',
+  data () {
+    return {
+      style: {
+        width: '100%',
+        height: 'calc(' + window.screen.height + 'px' + ' - 194px)',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column'
+      }
+    }
+  }
+}
+</script>

d87d2870519d881098ab79c5a7edb8a6e341ed8d
xujintao <1101916341@qq.com>
2020-05-13 17:42:49 +0800

    徐锦涛---风险保证金---0513

--- src/views/sealapproveform/index.vue
+++ src/views/sealapproveform/index.vue
@@ -1,7 +1,7 @@
 /** *@author XuJinTao *@date 2020/5/13 9:03 *@title index */
 <template>
   <div class="sealapproveform">
-    <div class="title">投标报价信息表详情</div>
+    <div class="title">详情</div>
     <van-field style="display: none;" name="id" v-model="useApprovalList.id" type="text" readonly />
     <van-field
       style="display: none;"

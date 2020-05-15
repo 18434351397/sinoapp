d46333636e8b1b8b62525c059ad348daf5455926
11943 <1194362549@qq.com>
2020-05-15 09:50:51 +0800

    车永基--印章销毁&发文管理创建目录--任务--0514

---
+++ src/views/postmanageform/index.vue
@@ -0,0 +1,18 @@
+/**
+*@author CheYongJi
+*@date 2020/5/15 9:40
+*@title index
+*/
+<template>
+    <div>发文管理</div>
+</template>
+
+<script>
+export default {
+  name: 'index'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/sealdestoryform/index.vue
@@ -0,0 +1,18 @@
+/**
+*@author CheYongJi
+*@date 2020/5/15 9:39
+*@title index
+*/
+<template>
+    <div>印章销毁</div>
+</template>
+
+<script>
+export default {
+  name: 'index'
+}
+</script>
+
+<style scoped>
+
+</style>

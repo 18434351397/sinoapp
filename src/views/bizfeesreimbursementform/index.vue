6788320311e3412c3aaddfbe3484b3e6a6c73079
11943 <1194362549@qq.com>
2020-05-19 18:15:09 +0800

    车永基--报销发起流程的绘制和调试--任务--0519

--- src/views/bizfeesreimbursementform/index.vue
+++ src/views/bizfeesreimbursementform/index.vue
@@ -5,44 +5,60 @@
 */
 <template>
   <div>
-    <div style="padding: 10px 15px;text-align: left;background-color: #fff;">个人借款信息</div>
+    <div style="padding: 10px 15px;text-align: left;background-color: #fff;">报销发起信息</div>
     <van-field
       style="display: none;"
       name="meetingUsers"
       v-model="custList.meetingUsers"
       type="text"
       readonly
-    /><van-field
+    />
+    <van-field
     style="display: none;"
     name="ownDeptId"
     v-model="custList.ownDeptId"
     type="text"
     readonly
-  />
+    />
+    <van-field
+    style="display: none;"
+    name="bankAccount"
+    v-model="custList.bankAccount"
+    type="text"
+    readonly
+    />
     <van-field
       style="display: none;"
       name="id"
       v-model="custList.id"
       type="text"
       readonly
-    /><van-field
+    />
+    <van-field
     style="display: none;"
-    name="company"
-    v-model="custList.company"
+    name="cou"
+    v-model="custList.cou"
     type="text"
     readonly
-  />
+    />
+    <van-field
+    style="display: none;"
+    name="payee"
+    v-model="custList.payee"
+    type="text"
+    readonly
+    />
     <van-field
       style="display: none;"
-      name="type"
-      v-model="custList.type"
+      name="oppenBank"
+      v-model="custList.oppenBank"
       type="text"
       readonly
     />
     <van-field
       style="display: none;"
-      name="loanUserId"
-      v-model="custList.loanUserId"
+      name="oppositeCompany"
+      v-model="custList.oppositeCompany"
       type="text"
       readonly
     />
@@ -163,8 +179,10 @@
           prop="aprUserIds">
         </el-table-column>
         <el-table-column
-          label="借款余额"
-          prop="totalRepayAmount">
+          label="借款余额">
+          <template slot-scope="scope">
+            {{totalLoanAmount}}
+          </template>
         </el-table-column>
         <el-table-column
           label="清借款金额"
@@ -176,6 +194,79 @@
         </el-table-column>
         <el-table-column
           label="查看明细">
+        </el-table-column>
+      </el-table>
+      <van-field
+        name="reimbursementAmount"
+        v-model="reimbursementAmount"
+        type="text"
+        label="报销总金额:"
+        readonly
+      />
+    </div>
+    <div>
+      <div style="border-top: 1px dashed #f8f8f8;padding: 10px 15px;text-align: left;background-color: #fff;">清借款列表</div>
+      <el-table
+        border
+        :data="feesReimbursementLoanVOList"
+        style="width: 100%">
+        <el-table-column type="index" label="序号" width="50" :index="indexMethods"></el-table-column>
+        <el-table-column
+          label="借款人"
+          prop="loanUserName">
+        </el-table-column>
+        <el-table-column
+          label="项目号"
+          prop="projectNo">
+        </el-table-column>
+        <el-table-column
+          label="项目名"
+          prop="projectName">
+        </el-table-column>
+        <el-table-column
+          label="待还金额"
+          prop="waitReAmount">
+        </el-table-column>
+        <el-table-column
+          label="借款编号"
+          prop="loanNo">
+        </el-table-column>
+        <el-table-column
+          label="科目"
+          prop="subjectNames">
+        </el-table-column>
+        <el-table-column
+          label="清借款金额"
+          prop="amounts">
+        </el-table-column>
+      </el-table>
+      <van-field
+        name="cleranloanAmount"
+        v-model="cleranloanAmount"
+        type="text"
+        label="清借款总金额:"
+        readonly
+      />
+      <van-field
+        name="payAmount"
+        v-model="payAmount"
+        type="text"
+        label="应付款金额:"
+        readonly
+      />
+    </div>
+    <div>
+      <div style="border-top: 1px dashed #f8f8f8;padding: 10px 15px;text-align: left;background-color: #fff;">附件列表</div>
+      <el-table border :data="file" style="width: 100%">
+        <el-table-column type="index" label="序号" width="50" :index="indexMethods"></el-table-column>
+        <el-table-column label="附件名称" prop="fileName"></el-table-column>
+        <el-table-column label="大小" width="80">
+          <template slot-scope="scope">{{ (scope.row.fileSize / 1024).toFixed(2) + 'KB' }}</template>
+        </el-table-column>
+        <el-table-column fixed="right" label="操作" width="50">
+          <template slot-scope="scope">
+            <el-button @click="handleClick(scope.row)" type="text" size="small">下载</el-button>
+          </template>
         </el-table-column>
       </el-table>
     </div>
@@ -191,7 +282,15 @@ export default {
     return {
       dataList: this.$route.query,
       custList: [],
-      costDetailList: []
+      costDetailList: [],
+      feesReimbursementLoanVOList: [],
+      reimbursementAmount: null, // 报销总金额
+      cleranloanAmount: null, // 清借款总金额
+      payAmount: null, // 应付款金额
+      totalLoanAmount: 0.00,
+      file: [],
+      fileList: [],
+      fileIdList: []
     }
   },
   created () {
@@ -201,21 +300,61 @@ export default {
     // 获取数据
     loadData () {
       bizfeesreimbursementformList(this.dataList.dataId).then(res => {
+        // 处理feetypedes
+        if (res.data.feesType === '1') {
+          res.data.feesTypeDes = '普通报销'
+        } else if (res.data.feesType === '2') {
+          res.data.feesTypeDes = '个人快速报销'
+        } else if (res.data.feesType === '3') {
+          res.data.feesTypeDes = '对公付款'
+        } else if (res.data.feesType === '4') {
+          res.data.feesTypeDes = '财务资金'
+        }
+        // 计算 报销总金额 清借款总金额 应付款金额 的字段
+        res.data.feesReimbursementDetailVOList.forEach(item => {
+          this.reimbursementAmount = (Number(this.reimbursementAmount) + Number(item.amount)).toFixed(2)
+          this.cleranloanAmount = (Number(this.cleranloanAmount + Number(item.loanAmount))).toFixed(2)
+          this.payAmount = (Number(this.reimbursementAmount) - Number(this.cleranloanAmount)).toFixed(2)
+        })
+
         this.custList = res.data
-        // this.custList.date = this.custList.date.split(' ')[0]
-        // this.custList.planReDate = this.custList.planReDate.split(' ')[0]
+        // 处理时间的格式
+        this.custList.reimbursementDate = this.custList.reimbursementDate.split(' ')[0]
         this.costDetailList = res.data.feesReimbursementDetailVOList
-        this.getselectByUserIdLoanList(this.custList.payee)
+        // 获取清借款列表并对借款金额进行计算
+        this.getselectByUserIdLoanList(res.data.payee)
+        // 清借款列表数据
+        this.feesReimbursementLoanVOList = res.data.feesReimbursementLoanVOList
+        // 附件上传列表数据
+        this.file = res.data.fileList
+        this.fileList = res.data.fileList
+        // 处理上传文件的fileIdList
+        this.fileList.forEach(item => {
+          this.fileIdList = this.fileIdList.concat(item.fileId)
+        })
+        // 处理上传文件的fileList
+        this.fileList = this.fileList.map(item => {
+          return JSON.stringify({
+            url: item.url,
+            fileName: item.fileName,
+            fileSize: item.fileSize,
+            fileId: item.fileId
+          })
+        })
       })
     },
     // 获取借款列表数据
     getselectByUserIdLoanList (userid) {
       selectByUserIdLoanList(userid).then(res => {
-
+        res.data.forEach(item => {
+          this.totalLoanAmount = (Number(this.totalLoanAmount) + Number(item.loanAmount))
+        })
+        this.totalLoanAmount = '￥' + this.totalLoanAmount.toFixed(2)
       })
     },
-    // 处理借款金额
-    handletotalRepayAmount () {
+    // 处理表格的序号的方法
+    indexMethods (index) {
+      return index + 1
     }
   }
 }

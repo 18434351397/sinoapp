02184e200815d41eb77127175eecea09d6310244
xujintao <xujintao@gmail.com>
2021-07-14 19:21:17 +0800

    bug修复

--- src/views/projpcontractreviewChange/SeeCashFlowTable/SeeCashFlowTable.vue
+++ src/views/projpcontractreviewChange/SeeCashFlowTable/SeeCashFlowTable.vue
@@ -25,7 +25,7 @@
       <vxe-table-column field="confirmAmount" width="160" title="现金流量金额"></vxe-table-column>
       <vxe-table-column field="occupyTime" width="160" title="资金占用时间"></vxe-table-column>
       <vxe-table-column field="occupyAmount" width="160" title="资金占用成本"></vxe-table-column>
-      <vxe-table-column field="remark" width="100" title="备注"></vxe-table-column>
+      <!-- <vxe-table-column field="remark" width="100" title="备注"></vxe-table-column> -->
     </vxe-table>
     <van-field type="text" v-model="occupy" label="资金占用情况" colon readonly>
       <template #input>
